58eafece1367f5626d49ae39e92a870b102c58dc
Rushikesh Chavan <rushikeshchavan318@gmail.com>
2023-08-16 10:37:14 +0530

    Dice Game

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,291 @@
-import Image from 'next/image'
+"use client";
+import React, { useState, useEffect } from "react";
 
-export default function Home() {
+const diceColors = {
+  1: "#EF4444",
+  2: "#EAB307",
+  3: "#22C55E",
+  4: "#3B82F6",
+  5: "#6366F1",
+  6: "#A855F7",
+};
+const DiceComponent = ({ index, diceValues }) => {
+  const [isRolling, setIsRolling] = useState(false);
+
+  useEffect(() => {
+    setIsRolling(true);
+
+    const timeout = setTimeout(() => {
+      setIsRolling(false);
+    }, 100);
+
+    return () => clearTimeout(timeout);
+  }, [diceValues]);
+
+  const animationStyle = {
+    transform: isRolling ? "rotate(-90deg)" : "rotate(90deg)",
+    transition: " transform 0.15s ",
+  };
+
+  const diceValue = diceValues[index];
+  const selectDice = () => {
+    switch (diceValue) {
+      case 1:
+        return (
+          <span className="absolute top-[40%] left-[40%] bg-[#713E12] rounded-full w-4 h-4" />
+        );
+      case 2:
+        return (
+          <>
+            <span className="absolute top-[15%] left-[15%]  bg-[#713E12] rounded-full w-4 h-4" />
+            <span className="absolute top-[65%] left-[65%]  bg-[#713E12] rounded-full w-4 h-4" />
+          </>
+        );
+      case 3:
+        return (
+          <>
+            <span className="absolute top-[15%] left-[15%]  bg-[#713E12] rounded-full w-4 h-4" />
+            <span className="absolute top-[40%] left-[40%]  bg-[#713E12] rounded-full w-4 h-4" />
+            <span className="absolute top-[65%] left-[65%]  bg-[#713E12] rounded-full w-4 h-4" />
+          </>
+        );
+      case 4:
+        return (
+          <>
+            <span className="absolute top-[15%] left-[15%] bg-[#713E12] rounded-full w-4 h-4" />
+            <span className="absolute top-[15%] left-[65%] bg-[#713E12] rounded-full w-4 h-4" />
+            <span className="absolute top-[65%] left-[15%] bg-[#713E12] rounded-full w-4 h-4" />
+            <span className="absolute top-[65%] left-[65%]  bg-[#713E12] rounded-full w-4 h-4" />
+          </>
+        );
+      case 5:
+        return (
+          <>
+            <span className="absolute top-[15%] left-[15%]  bg-[#713E12] rounded-full w-4 h-4" />
+            <span className="absolute top-[15%] left-[65%]  bg-[#713E12] rounded-full w-4 h-4" />
+            <span className="absolute top-[40%] left-[40%]  bg-[#713E12] rounded-full w-4 h-4" />
+            <span className="absolute top-[65%] left-[15%]  bg-[#713E12] rounded-full w-4 h-4" />
+            <span className="absolute top-[65%] left-[65%]  bg-[#713E12] rounded-full w-4 h-4" />
+          </>
+        );
+      case 6:
+        return (
+          <>
+            <span className="absolute top-[15%] left-[15%] bg-[#713E12] rounded-full w-4 h-4" />
+            <span className="absolute top-[15%] left-[65%] bg-[#713E12] rounded-full w-4 h-4" />
+            <span className="absolute top-[40%] left-[15%] bg-[#713E12] rounded-full w-4 h-4" />
+            <span className="absolute top-[40%] left-[65%] bg-[#713E12] rounded-full w-4 h-4" />
+            <span className="absolute top-[65%] left-[15%] bg-[#713E12] rounded-full w-4 h-4" />
+            <span className="absolute top-[65%] left-[65%] bg-[#713E12] rounded-full w-4 h-4" />
+          </>
+        );
+      default:
+        return <>Invalid</>;
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div style={{ ...animationStyle }}>
+      <div
+        className="w-20 h-20 rounded-lg relative"
+        style={{ backgroundColor: diceColors[index + 1] }}
+      >
+        {selectDice()}
+      </div>
+    </div>
+  );
+};
+
+const MultipleDiceComponent = () => {
+  const [diceValues, setDiceValues] = useState([6, 6, 6, 6, 6, 6]);
+  const [diceLength, setDiceLength] = useState(6);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [names, setNames] = useState(["Player 1", "Player 2"]);
+  const [currentName, setCurrentName] = useState("");
+  const [currentPlayerIndex, setCurrentPlayerIndex] = useState(0);
+
+  const rollDice = () => {
+    const randomValues = Array.from(
+      { length: diceLength },
+      () => Math.floor(Math.random() * 6) + 1
+    );
+    setDiceValues(randomValues);
+  };
+
+  const diceSum = diceValues.reduce((sum, value) => sum + value, 0);
+
+  const handleButtonClick = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+  const handleCloseDrawer = () => {
+    setIsDrawerOpen(false);
+  };
+
+  useEffect(() => {
+    rollDice();
+  }, [diceLength]);
+
+  const subDiceLength = () => {
+    setDiceLength(diceLength - 1);
+  };
+  const addDiceLength = () => {
+    setDiceLength(diceLength + 1);
+  };
+
+  const handleInputChange = (event) => {
+    const { value } = event.target;
+    setCurrentName(value);
+  };
+
+  const handleAddName = () => {
+    if (currentName.trim() !== "") {
+      setNames([...names, currentName.trim()]);
+      setCurrentName("");
+    }
+  };
+
+  const handleRemoveName = (index) => {
+    const itemToRemove = names[index];
+    setNames(names.filter((i) => i !== itemToRemove));
+  };
+
+  const playerCount = names.length;
+
+  const handleDiceClick = () => {
+    if (names.length > 0) {
+      const delayTime = 500;
+      setTimeout(() => {
+        setCurrentPlayerIndex((prevIndex) => (prevIndex + 1) % names.length);
+      }, delayTime);
+    }
+  };
+
+  return (
+    <div
+      className={
+        "flex flex-col gap-12 justify-center items-center w-full h-screen"
+      }
+    >
+      <div className="bg-[#E8E8E8] text-black rounded-lg">
+        <button
+          onClick={() => {
+            handleDiceClick();
+            rollDice();
+          }}
+          disabled={names.length === 0}
+        >
+          <div className={"font-bold py-2"}>
+            {names[currentPlayerIndex]}
+            <p className={"font-normal p-5"}>played</p>
+          </div>
+
+          {diceSum}
+
+          {diceValues.length > 0 && (
+            <div
+              className={
+                "flex flex-wrap justify-center px-4 py-4 gap-x-4 gap-y-4"
+              }
+            >
+              {diceValues.map((value, index) => (
+                <DiceComponent
+                  key={index}
+                  index={index}
+                  diceValues={diceValues}
+                />
+              ))}
+            </div>
+          )}
+
+          <div className={"py-5 space-y-5"}>
+            <p className="font-bold">
+              {names[(currentPlayerIndex + 1) % names.length]}
+            </p>
+            <p>plays next</p>
+          </div>
+        </button>
+      </div>
+      <div className={"text-center"}>
+        {!isDrawerOpen && (
+          <button
+            onClick={handleButtonClick}
+            className="text-white rounded-lg bg-[#B4530A] p-2 "
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+            Open Manager
+          </button>
+        )}
+        <div
+          className={`flex flex-col justify-items-center bg-slate-500 h-screen fixed left-0 w-full p-4 
+                ${
+                  isDrawerOpen ? "top-0" : "top-[100%]"
+                } transition-all ease-in-out duration-300`}
+        >
+          <button
+            onClick={handleCloseDrawer}
+            className="text-center mx-auto text-white rounded-lg bg-[#B4530A] p-2 "
+          >
+            {isDrawerOpen ? "Close Manager" : "Open Manager"}
+          </button>
+          <h2 className="text-center font-bold text-xl mx-auto py-5">Dices</h2>
+          <div className="flex justify-center h-8 bg-white mx-auto gap-4">
+            <button
+              disabled={diceLength === 1}
+              className="w-10 text-white bg-blue-700 hover:bg-blue-400 disabled:cursor-not-allowed"
+              onClick={subDiceLength}
+            >
+              -
+            </button>
+            <span className="text-black flex justify-cente mx-auto my-auto">
+              {diceLength}
+            </span>
+            <button
+              disabled={diceLength === 6}
+              className="w-10 text-white bg-blue-700 hover:bg-blue-400 disabled:cursor-not-allowed"
+              onClick={addDiceLength}
+            >
+              +
+            </button>
+          </div>
+          <div className="py-4 text-2xl">
+            <span className="font-bold text-xl">Players ({playerCount})</span>
+          </div>
+          <div className={"bg-white text-left"}>
+            <input
+              type="text"
+              className="w-full"
+              placeholder="Name"
+              value={currentName}
+              onChange={handleInputChange}
+              onKeyUp={(event) => {
+                if (event.key === "Enter") {
+                  handleAddName();
+                }
+              }}
             />
-          </a>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 absolute right-0 rounded-sm mx-5 w-10 text-white"
+              onClick={handleAddName}
+            >
+              +
+            </button>
+            <div className="border border-gray-300 rounded">
+              {names.map((name, index) => (
+                <div key={index} className="flex justify-between">
+                  <p>{name}</p>
+                  <button
+                    className="text-white hover:bg-red-300 bg-red-800 m-1 rounded-sm w-10"
+                    onClick={() => handleRemoveName(index)}
+                  >
+                    -
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default MultipleDiceComponent;
